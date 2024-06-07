988a111e4c6a9c6015079e26e6d0e0f0772eaf02
Daniel Correa <daniel.correa55137@gmail.com>
2024-06-07 18:37:12 +1000

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.yarnpetalsgc.com"),
@@ -43,6 +44,8 @@ export default function RootLayout({
         <Toaster />
         <Navbar />
         {children}
+        <Analytics />
+
         <Footer />
       </body>
     </html>
